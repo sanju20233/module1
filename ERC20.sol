bc831a36dc352e34af8f594f258fbe8421205b31
sanju20233 <130916841+sanju20233@users.noreply.github.com>
2024-04-01 23:37:00 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -5,8 +5,8 @@ contract ERC20 {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "Kuldeep";
-    string public symbol = "coin";
+    string public name = "SANJAY";
+    string public symbol = "SK";
     uint8 public decimals = 18;
 
 		event Transfer(address indexed from, address indexed to, uint value);
